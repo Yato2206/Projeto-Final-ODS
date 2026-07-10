5978af008cae2c5b08039dc97f8403c13f6def70
Ricardo Pinto <ricardo.daniel.fernandes199@gmail.com>
2026-07-10 21:08:42 +0100

    more useless files
    Co-Authored-By: Vasco Piloto <161583341+OtterTho@users.noreply.github.com>

--- backend/service/src/main/kotlin/pt/isel/ps/UpdatedDataEmitter.kt
+++
@@ -1,9 +0,0 @@
-package org.example
-
-interface UpdatedDataEmitter {
-    fun emit(signal: UpdatedData)
-
-    fun onCompletion(callback: () -> Unit)
-
-    fun onError(callback: (Throwable) -> Unit)
-}
